c52eb7284eb5a5e7675b5bf61a960e3afe2fe1ea
AlexAaltonen <alex.aaltonen99@gmail.com>
2025-09-30 13:49:37 +0300

    Funktio joka luo pelaajan palauttaa pelaajan_id ja aloitus lokaation.
    aloitus pisteet on 1000 --> voi muuttaa

--- main.py
+++ main.py
@@ -1,21 +1,8 @@
 import mysql.connector
-import random
+
 
 
 def add_player(name, starting_points=1000):
-    conn = mysql.connector.connect(
-        host='127.0.0.1',
-        port=3306,
-        database='flight_game',
-        user='Alex',
-        password='MasaRektaa69',
-        autocommit=True
-    )
-
-    cursor = conn.cursor()
-    cursor.execute("select ident from chosen_airports")
-    airports = cursor.fetchall()
-    starting_location = random.choice(airports)[0]
 
     sql = "insert into player (name, points, location) VALUES(%s, %s, %s)"
     cursor.execute(sql, (name, starting_points, starting_location))
